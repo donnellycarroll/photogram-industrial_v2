64fca161cb3950bf0856ab55e5f47d42d2d266e3
donnellycarroll <146589442+donnellycarroll@users.noreply.github.com>
2025-04-12 17:28:37 +0000

    some improvements, still cant get follow requests

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 Rails.application.routes.draw do
+  
   root "users#feed"
 
   devise_for :users
 
   resources :comments
   resources :follow_requests
-  resources :likes 
+  resources :likes
   resources :photos
+  resources :users, only: [ :index ]
 
   get ":username" => "users#show", as: :user
   get ":username/liked" => "users#liked", as: :liked
   get ":username/feed" => "users#feed", as: :feed
   get ":username/discover" => "users#discover", as: :discover
-  
 end
